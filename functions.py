86db6082535a89753fdad36a01bb00764ba5568c
EdoardoUlrichProverbio <proverbio.1658119@studenti.uniroma1.it>
2021-01-11 15:58:50 +0100

    Add files via upload

--- functions.py
+++ functions.py
@@ -194,7 +194,122 @@ def min_clicks2(G, df, pages, final_category_dict,v,category, init_set_pages):
     else:
         return i, f'Not Possible, total pages reached: {len(tot_pages_only_set)} out of {len(init_set_pages)}'
     
+##################################################################################################################################
+#############################################              RQ4               #####################################################
+##################################################################################################################################
+
+def hyper_remove(H, df, set_pages, final_category_dict , category1 , category2, u, v, counter):
+    i = 0 
     
+    try:
+        #page reached can be only in the subgraph
+        pages_reached = [n for n in H.neighbors(v) if n in set_pages]
+        tot_pages_reached = pages_reached
+        
+        #check if v and u are neighbours 
+        if u in H.neighbors(v):    
+            print(f'Path #{counter}: Shortest distance {i+1}')
+            counter += 1
+            #if v and u are neighbour we erase the edge (v,u)
+            df = df[(df['source'] != v) & (df['target'] != u)]
+        
+        #function keep going until has no more page to reach
+        while len(pages_reached) != 0:
+            i += 1
+            pages_reached, tot_pages_reached = iterations(set_pages, H, pages_reached, tot_pages_reached)
+            
+            #for every reached page we check if its neighbour is u
+            for x in pages_reached:
+                if u in H.neighbors(x):
+                    counter += 1
+                    print(f'Path #{counter}: Shortest distance {i+1}')
+                    #removing the hyperlink
+                    df = df[(df['source'] != x) & (df['target'] != u)]
+                    #run again the function  with the new edge dataset passing the current counter of hyperlinks removed
+                    hyper_removed = hyper_remove(H, df, new_set_pages, final_category_dict , category1 , \
+                                                 category2, u, v, counter)  
+
+    except TypeError:
+        pass
+    
+    hyper_removed = counter
+    if hyper_removed != 0:
+        return hyper_removed, f'Number of minimum hyperlink removed to disconnect {u} and {v}'
+    
+    else:
+        print(f'{v} and {u} are not connected by default')
+
+
+
+##################################################################################################################################
+#############################################              RQ5               #####################################################
+##################################################################################################################################
+
+def distance(G, df, v,  set_pages, page_list_temp):
+    
+    i = 1 
+    end = False
+    pages_reached = [n for n in G.neighbors(v) if n in set_pages]
+    tot_pages_reached = pages_reached
+    
+    #check if v is directly connected with category nodes in page_list_temp
+    for q in G.neighbors(v):
+        if q in page_list_temp:
+            end = True
+    
+    #end the loop either if there are no pages left or function reach the category
+    while len(pages_reached) != 0 and end != True :
+            i += 1
+            pages_reached, tot_pages_reached = iterations(set_pages, G, pages_reached, tot_pages_reached)
+            #for every page reached check if its neighbour are part of category nodes in page_list_temp
+            for x in pages_reached:
+                for q in G.neighbors(x):
+                    if q in page_list_temp:
+                         end = True
+                            
+    #if category is not found let's put the distance as 100000 (high number) and it means node is not linked
+    if end == True:
+        return i
+    else:
+        return 100000
+    
+
+
+
+
+    def category_distance(G, df, final_category_dict, C0, categories):
+    
+    #dictionary where the distances will be saved
+    dist_from_C0 = {}
+    
+    #nodes in C0
+    page_list0 = final_category_dict[C0]
+    #All nodes in G
+    set_pages = list(set(G.nodes))
+    
+    #for very category 
+    for i in categories:
+        page_list_temp = final_category_dict[i]
+        dist_from_C0[i] = [] 
+        
+        #for every node in C0
+        for x in page_list0:    
+            #here is computed the distance between the node in C0 and the category i
+            dist = distance(G, df, x , set_pages, page_list_temp)
+            print(dist)        
+            dist_from_C0[i].append(dist) 
+            
+        print(f' category: {i} completed')
+    
+    #converting dictionary in a DataFrame 
+    data_dist = pd.concat({key: pd.Series(value) for key, value in dist_from_C0.items()}, axis=1) 
+    
+    return data_dist
+
+
+
+
+
 ##################################################################################################################################
 #############################################              RQ6               #####################################################
 ##################################################################################################################################
@@ -268,3 +383,77 @@ def pagerank(n_iter, a, nodes, M, G1):
         
     return final_pagerank_output(G1,v)
     
+
+
+########Another approach functions
+
+
+    def retrieve_M (G1, a, b, row_of_zeroes ):
+
+    n = int(len(G1.nodes())/7)
+    M = np.zeros((n, n), dtype = np.float32 )  #initialize a matrix nxn (n = #nodes)
+    nodes = [node for node in G1.nodes()]             #list of nodes in our graph
+
+    #for each row that corresponds to each node
+    for i,source in tqdm(enumerate(G1.nodes())):
+        
+        
+        nodes_linked = [elem for elem in G1[source]]   #list of nodes that can be reached from that node
+        for target in nodes_linked:
+            
+            if (i >= a*n) and (i<(a+1)*n):
+                ind = nodes.index(target)
+                
+                if (ind >= b*n) and (ind <(b+1)*n):
+                    M[i-n*a][ind-n*b] = G1[source][target][0]['weight']  #I assign the weight that I previously calculated
+                                                                         #in the correct cell of the matrix
+                        
+                if i in row_of_zeroes:
+                    M[i-n*a] = round(1/(n*7),6) 
+        
+    return M
+                
+
+
+
+
+def pagerank2(n_iter, alpha, G1, row_of_zeroes):
+    
+    starting_page = random.choice(nodes)    #select a random page where to start random
+    v = np.zeros(len(nodes))       
+    v[nodes.index(starting_page)] = 1.0   #create a strating vector that has 1 only in the position of the page selected
+    n = int(len(G1.nodes())/7)
+    v = np.reshape(v, (7, n))      #the vector is split as well in seven parts
+    
+    v_temp = v    #copy of v to host tempporarly the results
+    
+    v_final = np.zeros(len(nodes)) 
+    v_final = np.reshape(v_final, (7, n))
+    
+    
+    for b in range(7):
+        for a in range(7):
+            M = retrieve_M(G1, a, b, row_of_zeroes )   #the submatrix M we are working with each time
+            v_temp[a] = v[a]
+            
+            for i in range(n_iter):                     # number of iterations
+                n = len(M)                                #number of nodes taken
+                P = (1/n)*alpha + (1-alpha)*M
+                v_temp[a] = v_temp[a] @ P            
+                
+            v_final[b] += v_temp[a] 
+
+    v_final = np.reshape(v_final, (1, n*7))
+    
+    return v                                #the final vector v will contain the probbaility of being at page i
+                      
+
+
+def order_categories(v, nodes, final_category_dict):
+    pagerank = defaultdict()
+    for category, list_of_pages in final_category_dict.items():
+        pagerank[category] = sum([v[nodes.index[page]] for page in list_of_pages])
+        
+    sorted_dict = {key: v for key, value in sorted(pagerank.items(), key=lambda item: item[1])}
+    
+    return sorted_dict.keys()
